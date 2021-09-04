6734073cd421bce3f29629bfe10f0435d606d1ae
Christoffer <links685@gmail.com>
2021-09-04 02:51:50 +0200

    mergezort is beast

--- test/SortingAlgos/SpeedComparison.java
+++ test/SortingAlgos/SpeedComparison.java
@@ -1,5 +1,6 @@
 package SortingAlgos;
 
+
 import org.junit.Test;
 
 import java.util.Random;
@@ -29,16 +30,17 @@ public class SpeedComparison {
     @Test
     public void testSpeedWithCustomNumbersAndBound() {
         double divider = 1000, startTime, endTime;
-        int[] array1 = createArray(50000, 25000);
-        int[] array2 = array1.clone();
+        int[] array1 = createArray(400_000_000, 25_500_000);
+        /**int[] array2 = array1.clone();
         int[] array3 = array1.clone();
         int[] array4 = array1.clone();
-
+**/
         startTime = System.currentTimeMillis();
-        Mergesort.sort(array3);
+        Mergesort.sort(array1);
         endTime = System.currentTimeMillis();
-        System.out.printf("MergeSort: %.3fs | isSorted = %s\n", (endTime - startTime) / divider, isSorted(array3));
+        System.out.printf("MergeSort: %.3fs | isSorted = %s\n", (endTime - startTime) / divider, isSorted(array1));
 
+        /**
         startTime = System.currentTimeMillis();
         InsertionSort.sort(array1);
         endTime = System.currentTimeMillis();
@@ -47,11 +49,12 @@ public class SpeedComparison {
         startTime = System.currentTimeMillis();
         SelectionSort.sort(array2);
         endTime = System.currentTimeMillis();
-        System.out.printf("BubbleSort: %.3fs | isSorted = %s\n", (endTime - startTime) / divider, isSorted(array2));
+        System.out.printf("SelectionSort: %.3fs | isSorted = %s\n", (endTime - startTime) / divider, isSorted(array2));
 
         startTime = System.currentTimeMillis();
         BubbleSort.sort(array4);
         endTime = System.currentTimeMillis();
         System.out.printf("BubbleSort: %.3fs | isSorted = %s\n", (endTime - startTime) / divider, isSorted(array4));
+         */
     }
 }
